fb2177b5c9eb6642fc0ae01655fe395d658906af
Pierre Minier <pierre.minier@bordeaux-inp.fr>
2021-04-16 23:14:37 +0200

    [Nouveau] Enregistrement dans la base de donnée d'un nouvel utilisateur

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(session(sess))
 app.get("/", async (req, res)=>{
   const pseudo_input = req.session.pseudo
   const password_input = req.session.password
+  const email_input = req.session.email
+  const signup = req.session.signup
 
   const db = await openDb()
 
@@ -42,25 +44,38 @@ app.get("/", async (req, res)=>{
   let data = {
     pseudo_error: 0,
     password_error: 0,
-    mail_error: 0,
-    signup: 0
   }
 
   let succes_login = 0
 
-  if (user != undefined) { // le pseudo renseigné est dans la BDD
-    if (user.u_password === password_input) {
-    succes_login = 1
-    req.session.u_id = user.u_id
+  if (signup == 0){ // tentative de connexion
+    if (user != undefined) { // le pseudo renseigné est dans la BDD
+      if (user.u_password === password_input) {
+      succes_login = 1
+      req.session.u_id = user.u_id
+      }
+      else  // Le mot de passe ne correspond pas
+        data.password_error = 1
     }
-    else  // Le mot de passe ne correspond pas
-      data.password_error = 1
-  }
-  else{
-    if (pseudo_input != undefined) // La session a en mémoire une tentative
-      data.pseudo_error = 1
+    else{
+      if (pseudo_input != undefined) // La session a en mémoire une tentative
+        data.pseudo_error = 1
+    }
   }
 
+  else if (signup == 1){ // tentative d'inscription
+    if (user != undefined) // pseudo déjà utilisé
+      data.pseudo_error = 2
+    else{
+      var date = new Date()
+      var options = {weekday: "long", year: "numeric", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit"}
+      date = date.toLocaleDateString("fr-FR", options)
+
+      const sql_insert = `INSERT INTO USERS (u_pseudo, u_mail, u_password, u_connexion) VALUES (?, ?, ?, ?)`
+      db.run(sql_insert,[pseudo_input, email_input, password_input, date])
+    }
+  }
+  // else: première fois que l'on charge la page dans cette session
 
   if (succes_login === 1)
     res.redirect("home")
@@ -88,6 +103,17 @@ app.get("/home", async (req, res) => {
 app.post("/login", (req, res) => {
   req.session.password = req.body.password_input
   req.session.pseudo = req.body.pseudo_input
+  req.session.signup = 0
+  res.redirect("/")
+})
+
+
+// Enregistre les données entrées pour une potentielle inscription
+app.post("/signup", async (req, res) => {
+  req.session.password = req.body.password_input
+  req.session.pseudo = req.body.pseudo_input
+  req.session.email = req.body.email_input
+  req.session.signup = 1
   res.redirect("/")
 })
 
