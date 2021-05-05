6b0a2acf90bd44219d04f310c8a92f27ebff469b
Pierre Minier <pierre.minier@bordeaux-inp.fr>
2021-05-05 22:24:01 +0200

    [Nouveau] Lanceur de l'application

---
+++ Start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+
+if [ "$1" = "db" ]; then
+	echo "Réinitialise la base de donnée"
+	node init.js
+
+elif [ "$1" = "ss" ]; then
+	if [ -e sessions ]; then
+		echo "Supprime la précédente session"
+		rm sessions
+	else
+		echo "Pas de précédente session à supprimer"
+	fi
+
+elif [ "$1" = "a" ]; then
+	echo "Réinitialise la base de donnée"
+	node init.js
+	if [ -e sessions ]; then
+		echo "Supprime la précédente session"
+		rm sessions
+	else
+		echo "Pas de précédente session à supprimer"
+	fi
+fi
+
+node app.js
